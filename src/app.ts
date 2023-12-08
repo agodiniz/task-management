54a673a7964fbfe18976b8f07204dff1d8eebca6
agodiniz <tmmidiatiago@gmail.com>
2023-12-08 08:37:55 -0300

    chore: connect database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
 public app: express.Application;
@@ -7,6 +8,22 @@ public app: express.Application;
 public constructor() {
   this.app = express();
   this.app.use(cors());
+
+  this.initMongoose();
+  this.connectDatabase();
+}
+
+private initMongoose(): void {
+  mongoose.set('runValidators', true);
+}
+
+private connectDatabase(): void {
+  mongoose.connect('mongodb+srv://agodiniz:T14JfL4QJc9dyCyd@cluster0.0kqvisl.mongodb.net/curso-javascript?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 }
 
 public listen(port: number): void {
